dee2eb3cefe70ae9eb48127a13ffef6d587510d2
chakrit <platukem@hotmail.com>
2015-09-25 10:45:42 +0700

    Sync สำเร็จแล้ว

--- app/src/main/java/kuff/chakrit/tongrestaurant/MainActivity.java
+++ app/src/main/java/kuff/chakrit/tongrestaurant/MainActivity.java
@@ -4,8 +4,19 @@ import android.database.sqlite.SQLiteDatabase;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -47,6 +58,74 @@ public class MainActivity extends AppCompatActivity {
             //Constant
             InputStream objInputStream = null;
             String strJSON = null;
+            String strUrlUser = "http://swiftcodingthai.com/24Sep/php_get_data_tong.php";
+            String strUrlFood = "http://swiftcodingthai.com/24Sep/php_get_data_food_tong.php";
+            HttpPost objHttpPost;
+
+            //1.Create InputStream
+            try {
+                HttpClient objHttpClient = new DefaultHttpClient();
+                if (intTimes != 1) {
+                    objHttpPost = new HttpPost(strUrlUser);
+                } else {
+                    objHttpPost = new HttpPost(strUrlFood);
+                }
+
+                HttpResponse objHttpResponse = objHttpClient.execute(objHttpPost);
+                HttpEntity objHttpEntity = objHttpResponse.getEntity();
+                objInputStream = objHttpEntity.getContent();
+
+            } catch (Exception e) {
+                Log.d("Rest", "InputStream ==>" + e.toString());
+            }
+
+
+            //2.Create strJSON
+            try {
+                BufferedReader objBufferedReader = new BufferedReader(new InputStreamReader(objInputStream, "UTF-8"));
+                StringBuilder objStringBuilder = new StringBuilder();
+                String strLine = null;
+
+                while ((strLine = objBufferedReader.readLine()) != null ) {
+                    objStringBuilder.append(strLine);
+                }   //while
+                objInputStream.close();
+                strJSON = objStringBuilder.toString();
+
+            } catch (Exception e) {
+                Log.d("Rest", "strJSON ==>" + e.toString());
+            }
+
+            //3.Update SQLite
+            try {
+                final JSONArray objJsonArray = new JSONArray(strJSON);
+
+                for (int i = 0; i < objJsonArray.length(); i++) {
+
+                    JSONObject object = objJsonArray.getJSONObject(i);
+
+                    if (intTimes < 1) {
+
+                        // For UserTABLE
+                        String strUser = object.getString("User");
+                        String strPassword = object.getString("Password");
+                        String strName = object.getString("Name");
+                        objUserTable.addNewUser(strUser, strPassword, strName);
+
+                    } else {
+
+                        //For foodTABLE
+                        String strFood = object.getString("Food");
+                        String strSource = object.getString("Source");
+                        String strPrice = object.getString("Price");
+                        objFoodTable.addNewFood(strFood, strSource, strPrice);
+
+                    }
+                }
+
+            } catch (Exception e) {
+                Log.d("Rest", "Update SQLite ==>" + e.toString());
+            }
 
 
             intTimes += 1;
